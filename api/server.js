238ceff3b8e320d9a1558086eb3c2981985b323a
Samsoucy <newf930@outlook.com>
2019-03-18 16:35:58 -0400

    added migrations and register and login endpoints

--- api/server.js
+++ api/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const configureMiddleware = require('./middleware.js');
 const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const usersRouter = require('../users/user-router.js');
+
+
 
 const server = express();
 
+
 configureMiddleware(server);
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
 
 
 module.exports = server;
--- migrations/20190318142433_users.js
+++ migrations/20190318142433_users.js
@@ -1,8 +1,33 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments();
   
-};
-
-exports.down = function(knex, Promise) {
+        tbl
+        .string('username', 200)
+        .notNullable()
+        .unique();
+        tbl
+        .string('password', 200)
+        .notNullable();
+        
+        tbl
+        .string("name", 128);
+        tbl
+        .integer("age");
+        tbl
+        .string("email", 200);
+        tbl
+        .string("location", 200);
+        tbl
+        .decimal("price");
+        tbl
+        .text('bio');
+        tbl
+        .boolean("expectant mother").defaultTo(false);
+    });
+  };
   
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
